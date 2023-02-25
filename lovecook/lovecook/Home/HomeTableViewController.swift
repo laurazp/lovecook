4bfbcd3099fac7ed44a2a53a9c3856cc0bb06361
laurazp <laurikazp@gmail.com>
2023-02-25 20:00:25 +0000

    Info item added for Home in navigation bar

--- lovecook/lovecook/Home/HomeTableViewController.swift
+++ lovecook/lovecook/Home/HomeTableViewController.swift
@@ -1,16 +1,15 @@
 
 import UIKit
 
-class RecipesTableViewController: UITableViewController {
-
+class HomeTableViewController: UITableViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        configureNavigationBar()
+    }
+    
+    func configureNavigationBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "info.circle"), style: .done, target: self, action: nil)
     }
 
     // MARK: - Table view data source
