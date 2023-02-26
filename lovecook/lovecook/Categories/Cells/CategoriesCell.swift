58cedd7f402cf42866411877389ee8de705f0a69
laurazp <laurikazp@gmail.com>
2023-02-26 23:25:45 +0000

    CategoriesCell added as custom UITableViewCell

---
+++ lovecook/lovecook/Categories/Cells/CategoriesCell.swift
@@ -0,0 +1,29 @@
+
+import UIKit
+
+class CategoriesCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitleLabel: UILabel!
+    
+    var categoriesViewController: CategoriesTableViewController = CategoriesTableViewController()
+    
+    var indexPath: IndexPath = IndexPath()
+    //weak var delegate: UITableViewDelegate!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        categoryTitleLabel.text = nil
+        categoryImage.image = nil
+    }
+    
+}
