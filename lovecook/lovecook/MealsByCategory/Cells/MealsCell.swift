fcf3f3691f9f7ab1757afdf81fbbc227a9cf4ad9
laurazp <laurikazp@gmail.com>
2023-03-18 09:57:40 +0000

    Favorites button added next to every meal

--- lovecook/lovecook/MealsByCategory/Cells/MealsCell.swift
+++ lovecook/lovecook/MealsByCategory/Cells/MealsCell.swift
@@ -5,10 +5,33 @@ class MealsCell: UITableViewCell {
 
     @IBOutlet weak var mealTitleLabel: UILabel!
     @IBOutlet weak var mealImageView: UIImageView!
+    @IBOutlet weak var favoritesButton: UIButton!
+    var favoriteSelected: Bool = true
     
     var indexPath: IndexPath = IndexPath()
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        configureFavoritesButton()
     }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let margins = UIEdgeInsets(top: 5, left: 8, bottom: 5, right: 8)
+              contentView.frame = contentView.frame.inset(by: margins)
+              contentView.layer.cornerRadius = 10
+    }
+    
+    private func configureFavoritesButton() {
+        //TODO:
+    }
+    
+//    @IBAction func favoriteButtonTapped(_ sender: UIButton) {
+//        favoriteSelected = !favoriteSelected
+//        if favoriteSelected {
+//            favoritesButton.setImage(UIImage(systemName: "heart.fill"), for: .selected)
+//        } else {
+//            favoritesButton.setImage(UIImage(named: "heart"), for: .normal)
+//        }
+//    }
 }
