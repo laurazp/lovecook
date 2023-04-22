606a8d6594c72e6edf86605658f01fc5987767af
laurazp <laurikazp@gmail.com>
2023-04-22 11:24:33 +0100

    Label added and code cleaned

--- lovecook/lovecook/Recipes/RecipesViewController.swift
+++ lovecook/lovecook/Recipes/RecipesViewController.swift
@@ -15,7 +15,6 @@ class RecipesViewController: UIViewController {
     @IBOutlet weak var recipeTagsLabel: UILabel!
     @IBOutlet weak var ingredientsCardView: UIView!
     @IBOutlet weak var instructionsCardView: UIView!
-    @IBOutlet weak var measurementsLabel: UILabel!
     @IBOutlet weak var recipeIngredientsLabel: UILabel!
     @IBOutlet weak var detailsStackView: UIStackView!
     @IBOutlet weak var recipesScrollView: UIScrollView!
@@ -38,6 +37,7 @@ class RecipesViewController: UIViewController {
         ingredientsCardView.layer.cornerRadius = 20
         ingredientsCardView.layoutMargins = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
         instructionsCardView.layer.cornerRadius = 20
+        instructionsCardView.layoutMargins = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
         setupLabelsHeightInsideCardViews()
     }
     
@@ -62,12 +62,9 @@ class RecipesViewController: UIViewController {
         recipeTitleLabel.textColor = UIColor.darkGray
         recipeCategoryLabel.text = "Category: " + String(describing: detail.strCategory)
         recipeAreaLabel.text = "Area: " + String(describing: detail.strArea ?? "")
-        //measurementsLabel.text = configureMeasurementsText(detail: detail)
-        //let ingredientsText = configureIngredientsText(detail: detail)
-        //recipeIngredientsLabel.text = ingredientsText
         let ingredientsText = configureIngredientsAndMeasurementsText(detail: detail)
         recipeIngredientsLabel.attributedText = ingredientsText
-        recipeInstructionsLabel.text = detail.strInstructions
+        recipeInstructionsLabel.text = "\(detail.strInstructions)\n"
         if detail.strTags != nil {
             recipeTagsLabel.text = "Tags:  " + String(describing: detail.strTags ?? "")
         } else {
@@ -100,32 +97,4 @@ class RecipesViewController: UIViewController {
         }
         return ingredientsAttributedString
     }
-    
-//    private func configureIngredientsText(detail: Recipe) -> String {
-//        var existingIngredients: String = ""
-//        let allIngredientsArray = [detail.strIngredient1, detail.strIngredient2, detail.strIngredient3, detail.strIngredient4, detail.strIngredient5, detail.strIngredient6, detail.strIngredient7, detail.strIngredient8, detail.strIngredient9, detail.strIngredient10, detail.strIngredient11, detail.strIngredient12, detail.strIngredient13, detail.strIngredient14, detail.strIngredient15, detail.strIngredient16,detail.strIngredient17, detail.strIngredient18, detail.strIngredient19, detail.strIngredient20]
-//
-//        for ingredient in allIngredientsArray {
-//            if ingredient != nil {
-//                if ingredient != "" && ingredient != " " {
-//                    existingIngredients += "\(ingredient!)\n"
-//                }
-//            }
-//        }
-//        return existingIngredients
-//    }
-//
-//    private func configureMeasurementsText(detail: Recipe) -> String {
-//        var existingMeasurements: String = ""
-//        let allMeasurementsArray = [detail.strMeasure1, detail.strMeasure2, detail.strMeasure3, detail.strMeasure4, detail.strMeasure5, detail.strMeasure6, detail.strMeasure7, detail.strMeasure8, detail.strMeasure9, detail.strMeasure10, detail.strMeasure11, detail.strMeasure12, detail.strMeasure13, detail.strMeasure14, detail.strMeasure15, detail.strMeasure16,detail.strMeasure17, detail.strMeasure18, detail.strMeasure19, detail.strMeasure20]
-//
-//        for measure in allMeasurementsArray {
-//            if measure != nil {
-//                if measure != "" && measure != " " {
-//                    existingMeasurements += "\(measure!)\n"
-//                }
-//            }
-//        }
-//        return existingMeasurements
-//    }
 }
